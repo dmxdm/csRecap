cc5ae045ff48854297da7cc4900bb3dde0b94e69
dmxdm <andre.maculan@gmail.com>
2022-05-31 22:16:33 -0300

    S05A53

--- Seção 5/Course/Course/Program.cs
+++ Seção 5/Course/Course/Program.cs
@@ -1,9 +1,90 @@
 ﻿using System;
-
+using System.Globalization;
 namespace Course {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            //Console.WriteLine("Entre os dados do produto:");
+            //Console.Write("Nome: ");
+            //string Nome = Console.ReadLine();
+            //Console.Write("Preço: ");
+            //double Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Produto p = new Produto(Nome, Preco);
+            //Produto p2 = new Produto();
+            //Produto p3 = new Produto { Nome = "TV", Preco = 500.00, Quantidade = 20 };
+
+            //Console.WriteLine();
+            //Console.WriteLine("Dados do produto: " + p);
+            //Console.WriteLine();
+            //Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            //int qte = int.Parse(Console.ReadLine());
+            //p.AdicionarProdutos(qte);
+            //Console.WriteLine();
+            //Console.WriteLine("Dados atualizados: " + p);
+            //Console.WriteLine();
+            //Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            //qte = int.Parse(Console.ReadLine());
+            //p.RemoverProdutos(qte);
+            //Console.WriteLine();
+            //Console.WriteLine("Dados atualizados: " + p);
+
+
+
+
+            //############### SOBRECARGA DE CONSTRUTORES
+
+            //Console.WriteLine("Entre os dados do produto:");
+            //Console.Write("Nome: ");
+            //string Nome = Console.ReadLine();
+            //Console.Write("Preço: ");
+            //double Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Produto p = new Produto(Nome, Preco);
+
+            //Console.WriteLine();
+            //Console.WriteLine("Dados do produto: " + p);
+            //Console.WriteLine();
+            //Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            //int qte = int.Parse(Console.ReadLine());
+            //p.AdicionarProdutos(qte);
+            //Console.WriteLine();
+            //Console.WriteLine("Dados atualizados: " + p);
+            //Console.WriteLine();
+            //Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            //qte = int.Parse(Console.ReadLine());
+            //p.RemoverProdutos(qte);
+            //Console.WriteLine();
+            //Console.WriteLine("Dados atualizados: " + p);
+
+
+            //#################### Contrutores
+
+            //Console.WriteLine("Entre os dados do produto:");
+            //Console.Write("Nome: ");
+            //string Nome = Console.ReadLine();
+            //Console.Write("Preço: ");
+            //double Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Console.Write("Quantidade no estoque: ");
+            //int Quantidade = int.Parse(Console.ReadLine());
+
+            //Produto p = new Produto(Nome,Preco,Quantidade);
+
+            //Console.WriteLine();
+            //Console.WriteLine("Dados do produto: " + p);
+            //Console.WriteLine();
+            //Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            //int qte = int.Parse(Console.ReadLine());
+            //p.AdicionarProdutos(qte);
+            //Console.WriteLine();
+            //Console.WriteLine("Dados atualizados: " + p);
+            //Console.WriteLine();
+            //Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            //qte = int.Parse(Console.ReadLine());
+            //p.RemoverProdutos(qte);
+            //Console.WriteLine();
+            //Console.WriteLine("Dados atualizados: " + p);
+
         }
     }
 }
