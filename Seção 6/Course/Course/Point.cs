c09670cd421f01a1b6bc0555b79844a99bc6c46e
dmxdm <andre.maculan@gmail.com>
2022-06-02 21:33:00 -0300

    S06A67

---
+++ Seção 6/Course/Course/Point.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Course {
+    struct Point {
+
+        public double X, Y;
+
+        public override string ToString() {
+            return "(" + X + ", " + Y + ")"; 
+        }
+
+    }
+}
